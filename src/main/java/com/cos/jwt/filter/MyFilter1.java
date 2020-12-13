01d468d075b5ba44590311295bed4e0437797f7b
kms20a <kms21@naver.com>
2020-12-13 18:55:12 +0900

    token 처리 Filter 구현

--- src/main/java/com/cos/jwt/filter/MyFilter1.java
+++ src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -1,12 +1,15 @@
 package com.cos.jwt.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MyFilter1 implements Filter{
 
@@ -16,5 +19,4 @@ public class MyFilter1 implements Filter{
 		System.out.println("필터1");
 		chain.doFilter(request, response);
 	}
-
 }
